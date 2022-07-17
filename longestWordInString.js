cfb8a80bf53f6a6ca6b5a9b6919b7b843e973bc7
TJ <thomasjjohnson9311@gmail.com>
2022-07-17 17:25:15 -0500

    find longest word in a string

---
+++ longestWordInString.js
@@ -0,0 +1,11 @@
+function findLongestWordLength(str) {
+    let words = str.split(" ")
+    let longestWord = 0
+    for (let i = 0; i < words.length; i++){
+        if (words[i].length > longestWord) {
+            longestWord = words[i].length
+        }
+    }
+    return longestWord;
+}
+console.log(findLongestWordLength("The boy got the basketball"));
